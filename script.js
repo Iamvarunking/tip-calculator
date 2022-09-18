5572c924a24c4ad5843342cd5cb90e02ec735e88
iamvarunking <iamavrunking610@gmail.com>
2022-09-18 19:22:35 +0530

    add some validations

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 
-const billInput = document.getElementById('billTotalInput');
+    const billInput = document.getElementById('billTotalInput');
 
-const tipInput = document.getElementById('tipInput');
+    const tipInput = document.getElementById('tipInput');
 
-const perPersonTotalDiv = document.getElementById('perPersonTotal');
+    const perPersonTotalDiv = document.getElementById('perPersonTotal');
 
-const numberOfPeopleDiv = document.getElementById('numberOfPeople');
+    const numberOfPeopleDiv = document.getElementById('numberOfPeople');
 
-let numberOfPeople = Number(numberOfPeopleDiv.innerText);
+    let numberOfPeople = Number(numberOfPeopleDiv.innerText);
 
-const calculateBill = () => {
-    let bill = Number(billInput.value);
-    let tipPercentage = Number(tipInput.value)/100;
-    let tipAmount =  tipPercentage * bill;
-    let total = bill + tipAmount;
-    const perPersonTotal = total/numberOfPeople;
-    perPersonTotalDiv.innerText = `${perPersonTotal.toFixed(2)}`;
-}
+    const clear = () => {
+        billInput.value = ''
+        tipInput.value = ''
+        perPersonTotalDiv.innerText = '₹0.00'
+        numberOfPeopleDiv.innerText = '1'
+    }
 
-const increasePeople = () => {
-    numberOfPeople += 1;
-    numberOfPeopleDiv.innerText = numberOfPeople
-    calculateBill();
+    const calculateBill = () => {
+        let bill = Number(billInput.value);
+        if(/[0-9]/.test(bill) == false ){
+            alert("please enter a valid amount")
+            clear()
+            return
+        }
+        let tip = Number(tipInput.value)
+        let tipPercentage = tip/100;
+        if(/[0-9]/.test(tip) == false ){
+            alert("please enter a valid percentage")
+            clear()
+            return
+        }
+        let tipAmount =  tipPercentage * bill;
+        let total = bill + tipAmount;
+        const perPersonTotal = total/numberOfPeople;
+        perPersonTotalDiv.innerText = `${perPersonTotal.toFixed(2)}`;
+    }
 
-}
-
-const decreasePeople = () => {
-    if(numberOfPeople > 1) {
-        numberOfPeople -= 1;
+    const increasePeople = () => {
+        numberOfPeople += 1;
         numberOfPeopleDiv.innerText = numberOfPeople
         calculateBill();
+
     }
-}
+
+    const decreasePeople = () => {
+        if(numberOfPeople > 1) {
+            numberOfPeople -= 1;
+            numberOfPeopleDiv.innerText = numberOfPeople
+            calculateBill();
+        }
+    }
